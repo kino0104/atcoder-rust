cf453a4d1d4ef60ed6ead81dd4305b21653aa30f
kino0104a <kyousuke.kinoshita@gmail.com>
2021-05-28 09:27:46 +0900

    abc201 a

---
+++ abc201/src/bin/a.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        mut a: [i32; 3],
+    }
+    a.sort();
+    if a[1] - a[0] == a[2] - a[1] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
